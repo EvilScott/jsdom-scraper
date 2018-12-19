2007ee7ad5ad0c65826893c6f6449ed49193a55e
Robert Scott Reis <reis.robert.s@gmail.com>
2018-12-19 15:33:29 -0800

    use request-promise-native to keep details about the request

--- index.js
+++ index.js
@@ -1,31 +1,55 @@
 const bodyParser = require('body-parser');
 const express = require('express');
+const request = require('request-promise-native');
 const { JSDOM } = require('jsdom');
 
-const PORT = 8080;
-const OPTIONS = {
+const DEFAULT_JSDOM_OPTIONS = {
   resources: 'usable',
   runScripts: 'dangerously',
 };
+const DEFAULT_REQUEST_OPTIONS = {
+  rejectUnauthorized: false,
+  resolveWithFullResponse: true,
+  simple: false,
+  time: true,
+};
+const DEFAULT_TIMEOUT = 5 * 1000;
+const PORT = 8080;
 
 const app = express();
 
 app.post('/scrape', bodyParser.json(), async (req, res) => {
   const url = req.body.url;
+  const timeout = parseInt(req.body.timeout) || DEFAULT_TIMEOUT;
   if (!url) {
     return res.sendStatus(422);
   }
   try {
+    // scrape URL and keep track of details
     console.log(`Scraping ${url}`);
-    const dom = await JSDOM.fromURL(url, OPTIONS);
+    const requestOpts = Object.assign({}, DEFAULT_REQUEST_OPTIONS, { timeout, url });
+    const {
+      request: { _rp_options: { url }, href: finalURL },
+      body,
+      statusCode,
+      timings: { end: responseTime },
+    } = await request(requestOpts);
+
+    // process scripts for final html
+    const jsdomOpts = Object.assign({}, DEFAULT_JSDOM_OPTIONS, { url: finalURL });
+    const dom = new JSDOM(body, jsdomOpts);
     const content = dom.serialize();
     console.log(`Found content length: ${content.length}`);
     dom.window.close();
-    res.send(content);
-  } catch (err) {
-    const errString = err.toString();
-    console.log(errString);
-    res.status(500).send(errString);
+    res.json({
+      content,
+      finalURL,
+      statusCode,
+      timing: Math.round(responseTime),
+      url,
+    });
+  } catch (error) {
+    res.status(500).json({ error });
   }
 });
 
